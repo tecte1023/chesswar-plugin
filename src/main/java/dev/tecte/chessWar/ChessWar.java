6fa6f59007a9a3e53b104c76e14e79175ea4adf2
tecte <tecte1023@gmail.com>
2025-07-28 22:35:48 +0900

    feat: Initial project setup
    - gradle 설정
    - 메인 클래스(ChessWar.java) 추가
    - plugin.yml 추가
    - .gitignore 설정

---
+++ src/main/java/dev/tecte/chessWar/ChessWar.java
@@ -0,0 +1,15 @@
+package dev.tecte.chessWar;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChessWar extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
