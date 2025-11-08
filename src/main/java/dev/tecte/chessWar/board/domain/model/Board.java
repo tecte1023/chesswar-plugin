adf5533dbed646a8d19c476f1038d4403b4add02
tecte <tecte1023@gmail.com>
2025-11-09 02:10:55 +0900

    feat(game): Implement sequential piece spawning to prevent lag
    - GameService에서 BukkitRunnable을 사용하여 몹 소환 작업을 여러 틱에 분산시켜 서버 렉을 방지
    - Board에 몹 소환 위치와 방향 계산을 위한 헬퍼 메소드 추가
    - TeamDirectionPolicy를 구현하여 팀과 보드 방향에 따라 기물이 바라보는 방향을 결정
    - MythicMobsPieceLayoutLoader에서 흑과 백 진영의 시작 위치를 올바르게 수정

--- src/main/java/dev/tecte/chessWar/board/domain/model/Board.java
+++ src/main/java/dev/tecte/chessWar/board/domain/model/Board.java
@@ -1,6 +1,8 @@
 package dev.tecte.chessWar.board.domain.model;
 
 import lombok.Builder;
+import lombok.NonNull;
+import org.bukkit.util.Vector;
 
 import java.util.Objects;
 
@@ -19,5 +21,29 @@ public record Board(
         Objects.requireNonNull(squareGrid, "Square grid cannot be null");
         Objects.requireNonNull(innerBorder, "Inner border cannot be null");
         Objects.requireNonNull(frame, "Frame cannot be null");
+    }
+
+    /**
+     * 지정된 좌표에 대한 기물의 스폰 위치 벡터를 계산합니다.
+     *
+     * @param coordinate 스폰할 칸의 좌표
+     * @return 스폰 위치를 나타내는 {@link Vector} 객체
+     */
+    @NonNull
+    public Vector spawnPositionVector(@NonNull Coordinate coordinate) {
+        return squareGrid.squareAt(coordinate)
+                .boundingBox()
+                .getCenter()
+                .add(new Vector(0, 1, 0));
+    }
+
+    /**
+     * 보드의 방향 정보를 반환합니다.
+     *
+     * @return 보드의 {@link Orientation}
+     */
+    @NonNull
+    public Orientation orientation() {
+        return squareGrid.orientation();
     }
 }
