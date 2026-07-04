ec4625515c804638f8f2b361a60c5fcf17435ec2
tecte <tecte1023@gmail.com>
2026-07-05 06:45:26 +0900

    refactor(economy): replace global economy state with granular components
    - 전역으로 묶여있던 경제 상태 객체를 제거하고 개별 기물 단위의 독립적인 골드 컴포넌트로 개편
    - 골드 수급 및 소비 과정에서 발생할 수 있는 상태 불일치를 원천 차단하여 상점 이용 및 재화 관리의 신뢰도를 높임

--- src/main/java/dev/tecte/chesswar/economy/GoldComponent.java
+++ src/main/java/dev/tecte/chesswar/economy/GoldComponent.java
@@ -1,38 +1,15 @@
 package dev.tecte.chesswar.economy;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 import lombok.experimental.Accessors;
 
-/**
- * [순수 데이터 객체] 재화 상태를 저장하는 컴포넌트.
- */
-@Data
+@Getter
+@Setter(AccessLevel.PACKAGE)
 @Accessors(fluent = true)
-@NoArgsConstructor
-@AllArgsConstructor(staticName = "of")
+@AllArgsConstructor
 public class GoldComponent {
     private int currentGold;
-    private int baseIncome;
-
-    public static GoldComponent createDefault() {
-        return GoldComponent.of(0, 50); // 기본 월급 50골드
-    }
-
-    public void add(int amount) {
-        this.currentGold += amount;
-    }
-
-    public boolean subtract(int amount) {
-        if (this.currentGold < amount) {
-            return false;
-        }
-        this.currentGold -= amount;
-        return true;
-    }
-
-    public boolean has(int amount) {
-        return this.currentGold >= amount;
-    }
 }
--- src/main/java/dev/tecte/chesswar/economy/GoldSource.java
+++ src/main/java/dev/tecte/chesswar/economy/GoldSource.java
@@ -4,17 +4,15 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.Accessors;
 
-/**
- * [순수 데이터 객체] 재화 획득 경로 정의.
- */
 @Getter
 @Accessors(fluent = true)
 @RequiredArgsConstructor
 public enum GoldSource {
-    SALARY("월급"),
-    KILL("처치 보상"),
-    TAX("세금"),
-    STEAL("탈취");
+    STIPEND("봉급", 50),
+    BOUNTY("포상금", 200),
+    LEVY("징세", 100),
+    PLUNDER("약탈금", 100);
 
     private final String description;
+    private final int amount;
 }
